3d3a6627be265b6bfb4cbc2d59d46c9ffb05f253
Gent Hoxha <gent.hoxha@scoopandspoon.com>
2018-10-24 17:20:13 +0200

    "sets"

---
+++ src/Mixed/sortedcollections/StockItem.java
@@ -0,0 +1,101 @@
+package Mixed.sortedcollections;
+
+public class StockItem {
+
+    private final String name;
+    private double price;
+    private int quantityStock = 0;
+
+    public StockItem(String name, double price) {
+        this.name = name;
+        this.price = price;
+        this.quantityStock = 0;
+    }
+
+    public StockItem(String name, double price, int quantityStock) {
+        this.name = name;
+        this.price = price;
+        this.quantityStock = quantityStock;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int quantityInStock() {
+        return quantityStock;
+    }
+
+    public void setPrice(double price) {
+        if (price > 0.0) {
+            this.price = price;
+        }
+    }
+    public void adjustStock(int quantity) {
+        int newQuantity = this.quantityStock + quantity;
+        if (newQuantity >= 0) {
+            this.quantityStock = newQuantity;
+        }
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        System.out.println("Entering StockItem.equals");
+        if (obj == this) {
+            return true;
+        }
+        if (obj == null || (obj.getClass()) != this.getClass()) {
+            return false;
+        }
+        String objName = ((StockItem) obj).getName();
+        return this.name.equals(objName);
+    }
+
+    @Override
+    public int hashCode() {
+        return this.name.hashCode() + 31;
+    }
+
+    public int CompareTo(StockItem stockItem) {
+        System.out.println("Entering StockItem.compareTo");
+        if (this == stockItem) {
+            return 0;
+        }
+        if (stockItem != null) {
+            return this.name.compareTo(stockItem.getName());
+        }
+        throw new NullPointerException();
+    }
+
+    public String toString() {
+        return this.name + " : price" + this.price;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
